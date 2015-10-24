a889c27e6077711ebf9c3a54ecd595428fe4f383
John P Bloch <johnpbloch@gmail.com>
2015-10-24 16:13:56 -0500

    Also need jquery

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title') - BudgeIt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="{{ elixir('js/all.js') }}"></script>
 </head>
 <body>
