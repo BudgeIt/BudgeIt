fb53db92ac3710effe6cc49e4bc60f18fff9dbcc
John P Bloch <johnpbloch@gmail.com>
2015-10-24 11:53:22 -0500

    Add nav bar to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,33 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#navigation-links" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">BudgeIt</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navigation-links">
+                <ul class="nav navbar-nav">
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    @if(Auth::check())
+                    @else
+                        <li><a class="register" href="/auth/register">Register</a></li>
+                        <li><a class="log-in" href="/auth/login">Log In</a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
 
 </body>
 </html>
