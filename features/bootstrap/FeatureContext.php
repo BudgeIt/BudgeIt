3e1b9d55aace71cae361bbbe527595812fc943d9
John P Bloch <johnpbloch@gmail.com>
2015-10-24 14:06:43 -0500

    Add a migrator context for auth

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,9 +3,12 @@
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\MinkExtension\Context\MinkContext;
+use Laracasts\Behat\Context\Migrator;
 
 class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
+
+    use Migrator;
 
     /**
      * @Given I am a guest
