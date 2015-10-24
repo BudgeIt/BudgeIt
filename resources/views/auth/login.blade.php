0e3b40070db7b63ccde40a087f2e4c0327360530
John P Bloch <johnpbloch@gmail.com>
2015-10-24 13:30:57 -0500

    Add form fields for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,28 @@
 @section('content')
     <div class="col-md-6 col-md-offset-3">
         <h1>Log In</h1>
+
         <form method="POST" action="/auth/login" id="log-in">
             {!! csrf_field() !!}
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" class="form-control" placeholder="Email" name="email"
+                       value="{{ old('email') }}">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Password" id="password">
+            </div>
+
+            <div class="checkbox">
+                <label for="remember">
+                    <input type="checkbox" name="remember" id="remember"> Remember Me
+                </label>
+            </div>
+
+            <button type="submit" class="btn btn-default" id="submit">Log In</button>
         </form>
     </div>
 @endsection
