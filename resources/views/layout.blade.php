f635ed7b30be03accdcb299c5543517f41653132
John P Bloch <johnpbloch@gmail.com>
2015-10-24 11:51:03 -0500

    Include main css file in layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>BudgeIt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
